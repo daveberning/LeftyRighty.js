4743fc4bb17054b2695b63cd2723dff35d8de3c0
Dave Berning <daveberning@Daves-MacBook-Pro.local>
2016-06-24 15:45:39 -0400

    Oops, forgot variable
    N00b

--- js/lefty-righty.js
+++ js/lefty-righty.js
@@ -26,8 +26,10 @@ function reverseAll() { // reverse children of lr and it's children and it's chi
 function reverseFirstLast() { // push children into array, flip first and last
     $.fn.flipFirstLast = function() {
         return this.each(function() {
+            var $this = $(this);
             var lrItem = $this.children('.lr-item');
             var lrItemCount = $this.children('.lr-item').length;
+
             var itemArray = [];
 
             lrItem.each(
