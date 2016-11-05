3dc43c18e9df106f2bb52045199f5c1f6d8412e3
Dave Berning <daveberning@icloud.com>
2016-11-05 17:58:23 -0400

    Cleanup

--- js/lefty-righty.js
+++ js/lefty-righty.js
@@ -12,21 +12,20 @@ var specificItem = '.lr-spec';
 var rowChild = '.lr-item'; // targets all bootstrap columns, default off
 
 function init() {
-    $('a').on('click',function() {
+    $('.lefty-righty').on('click',function() {
         reverseAll();
         reverseFirstLast();
         reverseSpecific();
     });
 }
+
 function reverseAll() { // reverse children of lr and it's children and it's children
     var lrSpecificItemCount = $(rowReverse).children(specificItem).length;
     var lrItemCount = $(rowReverse).children(rowChild).length;
 
     $.fn.reverseChildren = function() {
-
         return this.each(function(){
             // reverse ONLY lr-specific children
-
             if (lrSpecificItemCount) {
                 var $this = $(this);
                 var lrItem = $this.children(rowChild);
@@ -40,18 +39,11 @@ function reverseAll() { // reverse children of lr and it's children and it's chi
                         return itemArray;
                     }
                 );
-
-                console.log($(rowChild).index($(specificItem)));
-
-                console.log(specificItem);
-                console.log("specific item is present");
-
             } else {
                 // reverse every child
                 var $this = $(this);
                 $this.children(rowChild).each(function(){
                     $this.prepend(this);
-                    console.log("specific item is NOT present");
                 });
             }
 
@@ -90,7 +82,6 @@ function reverseFirstLast() { // push children into array, flip first and last
 }
 
 function reverseSpecific() { // flip specific divs
-
     $.fn.reverseSpecificItem = function() {
         return this.each(function() {
             var $this = $(this);
@@ -116,10 +107,8 @@ function reverseSpecific() { // flip specific divs
             $(firstArrayItem).before(betweenFirstLast);
         });
    }
-
    $(rowReverse).reverseSpecificItem();
 }
-
 
 (function ($) {
     $.fn.leftyrighty = function(options) {
@@ -127,21 +116,18 @@ function reverseSpecific() { // flip specific divs
 
         // This is the easiest way to have default options.
         var defaults = $.extend({
-            color: "black",
-            backgroundColor: "white",
-            bootstrapSupport: false
+            bootstrap: false,
+            showToggleButton: true
         }, options);
 
         var settings = $.extend({}, defaults, options);
 
-        // // Greenify the collection based on the settings variable.
-        // return this.css({
-        //     color: settings.color,
-        //     backgroundColor: settings.backgroundColor,
-        // });
-
-        if(settings.bootstrapSupport) { // option to check if dev is using BS
+        if(settings.bootstrap) { // option to check if dev is using BS
             rowChild = '[class^="col-"]';
+        }
+
+        if (!settings.showToggleButton) { // option to check if dev is using BS
+            $(".toggle").css("display", "none");
         }
     };
 }(jQuery));
